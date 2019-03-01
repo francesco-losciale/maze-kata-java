aee0d89922c066ea37c915d75c0f2c217eeeddf2
ealfors <francesco.losciale@ericsson.com>
2019-03-01 18:55:13 +0000

    Just save partial work

--- src/main/java/com/katas/maze/Player.java
+++ src/main/java/com/katas/maze/Player.java
@@ -15,6 +15,58 @@ public class Player {
     }
 
     public void executeInstruction(Character instruction) {
+        Position newPosition = executePositionMovement(instruction);
+        this.position = newPosition;
+        this.grid.markCellUsed(newPosition);
+    }
+
+    public Position getPosition() {
+        return this.position;
+    }
+
+    public Position find(Position endPosition, List<Position> visitedPositionList) {
+        for (String instruction : getPossibleOtherCells(visitedPositionList)) {
+            executePositionMovement(instruction.charAt(0)); // TODO fix this...
+            if (this.position != endPosition) {
+                visitedPositionList.add(this.position);
+                return find(endPosition, visitedPositionList);
+            } else {
+                return endPosition;
+            }
+        }
+        return this.position;
+    }
+
+    public List<String> getPossibleOtherCells(List<Position> visitedPositionList) {
+        List<String> result = new ArrayList<String>();
+        try {
+            if (!visitedPositionList.contains(this.position.moveEast())) {
+                result.add("E");
+            }
+        } catch (RuntimeException e) {
+        }
+        try {
+            if (!visitedPositionList.contains(this.position.moveWest())) {
+                result.add("W");
+            }
+        } catch (RuntimeException e) {
+        }
+        try {
+            if (!visitedPositionList.contains(this.position.moveNorth())) {
+                result.add("N");
+            }
+        } catch (RuntimeException e) {
+        }
+        try {
+            if (!visitedPositionList.contains(this.position.moveSouth())) {
+                result.add("S");
+            }
+        } catch (RuntimeException e) {
+        }
+        return result;
+    }
+
+    private Position executePositionMovement(Character instruction) {
         Position newPosition = this.position;
         if (instruction == 'E') {
             newPosition = this.position.moveEast();
@@ -31,46 +83,7 @@ public class Player {
         if (this.grid.isPositionAgainstWall(newPosition)) {
             throw new RuntimeException("Wall Found!");
         }
-        this.position = newPosition;
-        this.grid.markCellUsed(newPosition);
-    }
-
-    public Position getPosition() {
-        return this.position;
-    }
-
-    public Position findEndPosition() {
-        Position endPosition = Position.createPosition(this.grid, "0 0");
-        return endPosition;
-    }
-
-    public List<Character> getPossibleOtherCells(List<Position> visitedPositionList) {
-        List<Character> result = new ArrayList<Character>();
-        try {
-            if (!visitedPositionList.contains(this.position.moveEast())) {
-                result.add('E');
-            }
-        } catch (RuntimeException e) {
-        }
-        try {
-            if (!visitedPositionList.contains(this.position.moveWest())) {
-                result.add('W');
-            }
-        } catch (RuntimeException e) {
-        }
-        try {
-            if (!visitedPositionList.contains(this.position.moveNorth())) {
-                result.add('N');
-            }
-        } catch (RuntimeException e) {
-        }
-        try {
-            if (!visitedPositionList.contains(this.position.moveSouth())) {
-                result.add('S');
-            }
-        } catch (RuntimeException e) {
-        }
-        return result;
+        return newPosition;
     }
 
 }
--- src/test/java/com/katas/maze/PlayerTest.java
+++ src/test/java/com/katas/maze/PlayerTest.java
@@ -5,13 +5,17 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 
+@SuppressWarnings("ALL")
+@RunWith(JUnitParamsRunner.class)
 public class PlayerTest {
 
     @Test
@@ -39,12 +43,9 @@ public class PlayerTest {
         Player player = new Player(grid, playerPosition);
         List<Position> visitedPositionList = new ArrayList<Position>();
         visitedPositionList.add(playerPosition);
-        player.getPossibleOtherCells(visitedPositionList).
-        assertEquals(expectedPossibleMovements, String.join(" ", );
-        assertTrue(player.getPossibleOtherCells(visitedPositionList).contains('E'));
-        assertTrue(player.getPossibleOtherCells(visitedPositionList).contains('N'));
-        assertTrue(player.getPossibleOtherCells(visitedPositionList).contains('W'));
-        assertTrue(player.getPossibleOtherCells(visitedPositionList).contains('S'));
+        List<String> possibleOtherCellsList = player.getPossibleOtherCells(visitedPositionList);
+        Collections.sort(possibleOtherCellsList);
+        assertEquals(expectedPossibleMovements, String.join(" ", possibleOtherCellsList));
     }
 
     @Test
@@ -56,10 +57,10 @@ public class PlayerTest {
         visitedPositionList.add(playerPosition);
         // cannot go east
         visitedPositionList.add(Position.createPosition(grid, "0 0"));
-        assertFalse(player.getPossibleOtherCells(visitedPositionList).contains('E'));
-        assertTrue(player.getPossibleOtherCells(visitedPositionList).contains('N'));
-        assertTrue(player.getPossibleOtherCells(visitedPositionList).contains('W'));
-        assertTrue(player.getPossibleOtherCells(visitedPositionList).contains('S'));
+        assertFalse(player.getPossibleOtherCells(visitedPositionList).contains("E"));
+        assertTrue(player.getPossibleOtherCells(visitedPositionList).contains("N"));
+        assertTrue(player.getPossibleOtherCells(visitedPositionList).contains("W"));
+        assertTrue(player.getPossibleOtherCells(visitedPositionList).contains("S"));
     }
 
     @Test
@@ -68,8 +69,12 @@ public class PlayerTest {
         final String endPosition = "2 2";
         Grid grid = new Grid("3 3");
         Position playerPosition = Position.createPosition(grid, initialPosition);
+        List<Position> visitedPositionList = new ArrayList<Position>();
+        visitedPositionList.add(playerPosition);
         Player player = new Player(grid, playerPosition);
-        assertEquals(Position.createPosition(grid, endPosition), player.findEndPosition());
+        assertEquals(Position.createPosition(grid, endPosition),
+                player.find(Position.createPosition(grid, endPosition),
+                        visitedPositionList));
     }
 
 }
